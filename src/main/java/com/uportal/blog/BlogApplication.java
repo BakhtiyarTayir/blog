ca7d2accc64c473b3f0a556678b48696cf063045
BakhtiyarTayir <BakhtiyarTayir@gmail.com>
2023-01-25 17:31:34 +0500

    add Blog controller

--- src/main/java/com/uportal/blog/BlogApplication.java
+++ src/main/java/com/uportal/blog/BlogApplication.java
@@ -1,11 +1,12 @@
 package com.uportal.blog;
 
+import com.uportal.blog.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
